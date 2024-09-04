86d15286af6b83acbbcd4c7babed5201fd6fe32f
Lee-d0 <marmar11137@gmail.com>
2024-09-03 20:17:49 -0400

    Homepage signuppage loginpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import LogInPage from "./pages/auth/login/LogInPage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
+import Sidebar from "./componets/common/SideBar.jsx"
+import RightPanel from "./componets/common/RightPanel.jsx"
+import NotificationPage from "./pages/notification/NotificationPage.jsx"
+import ProfilePage from "./pages/profile/ProfilePage.jsx"
+
 
 
 function App() {
@@ -9,11 +14,16 @@ function App() {
 
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
       <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/login" element={<LogInPage/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/notifications" element={<NotificationPage/>}/>
+          <Route path="/profile/:username" element={<ProfilePage/>}/>
       </Routes>
+      <RightPanel />
+      
     </div>
       
     
